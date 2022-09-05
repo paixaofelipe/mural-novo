b424f95baea3c99911fea4c445495b51c083b392
SorayaExE <70041574+SorayaExE@users.noreply.github.com>
2022-09-05 20:49:33 -0300

    Update mural novo.py

--- mural novo.py
+++ mural novo.py
@@ -2,4 +2,6 @@ from time import sleep
 
 print("Felipe Paixão")
 print("Espero que eu já esteja para virar um cientista de dados!")
+print("Júlio César")
+print("Esperto que eu esteja empregado em uma empresa de TI até o final do curso!")
 sleep(1)
