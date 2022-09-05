2ab057980df2be52832d6ada05bda1b289720b5a
Cauamat <86753304+Cauamat@users.noreply.github.com>
2022-09-05 20:52:56 -0300

    Update mural novo.py

--- mural novo.py
+++ mural novo.py
@@ -3,4 +3,5 @@ from time import sleep
 print("Felipe Paixão")
 print("Espero que eu já esteja para virar um cientista de dados!")
 print("Thais Faustino: espero estar formada em TI e trabalhando como programadora há um boom tempo")
+print("Cauã Maturana: Daqui a 5 anos eu vou ver isso!")
 sleep(1)
