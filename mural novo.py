b3eb309b37266f8620dcc543fe616f8f8c2eb4ee
Thaís Faustino <108579118+thaiisfaustino@users.noreply.github.com>
2022-09-05 20:51:30 -0300

    Create mural novo.py

--- mural novo.py
+++ mural novo.py
@@ -2,6 +2,5 @@ from time import sleep
 
 print("Felipe Paixão")
 print("Espero que eu já esteja para virar um cientista de dados!")
-print("Júlio César")
-print("Esperto que eu esteja empregado em uma empresa de TI até o final do curso!")
+print("Thais Faustino: espero estar formada em TI e trabalhando como programadora há um boom tempo")
 sleep(1)
