6178a50df391c85e031ce2475b15809cf4b3e813
Othoniel1970 <95751672+Othoniel1970@users.noreply.github.com>
2022-09-05 20:51:58 -0300

    Update mural novo.py

--- mural novo.py
+++ mural novo.py
@@ -2,5 +2,9 @@ from time import sleep
 
 print("Felipe Paixão")
 print("Espero que eu já esteja para virar um cientista de dados!")
-print("Thais Faustino: espero estar formada em TI e trabalhando como programadora há um boom tempo")
+print("Júlio César")
+print("Esperto que eu esteja empregado em uma empresa de TI até o final do curso!")
 sleep(1)
+print("Othoniel")
+print("Vai Corinthians!!!")
+Sleep(2)
